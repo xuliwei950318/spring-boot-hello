5d1c0e3dc17eb0afa839de36215c48f0b0eaac43
xuliwei950318 <1051597421@qq.com>
2020-05-10 15:59:19 +0800

    天天

---
+++ src/main/java/springboot/example/Main.java
@@ -0,0 +1,10 @@
+package springboot.example;
+
+/**
+ * Created by xuliwei on 2020/5/10.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("jij");
+    }
+}
