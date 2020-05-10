3395e99254032c3c857aefbfa8a6ad2cc740e29c
xuliwei950318 <1051597421@qq.com>
2020-05-10 16:09:48 +0800

    ff

--- src/main/java/springboot/example/Main.java
+++ src/main/java/springboot/example/Main.java
@@ -5,6 +5,6 @@ package springboot.example;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("jij");
+        System.out.println("jij个股");
     }
 }
